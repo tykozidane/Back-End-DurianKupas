715471fa97f349231806f1f629e8998b3c8a1eb7
tykozidane2015 <tykozidane2015@gmail.com>
2022-06-07 17:41:25 +0700

    img product

--- models/Products.js
+++ models/Products.js
@@ -4,7 +4,7 @@ const productSchema = new mongoose.Schema({
     nama: {type: String, required: true},
     deskripsi: {type: String, required: true},
     harga: {type: Number, required: true},
-
+    img: {type: String, required: true}
 }, {timestamps: true});
 
 module.exports = mongoose.model("Product", productSchema);
