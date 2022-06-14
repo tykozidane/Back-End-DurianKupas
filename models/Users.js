7519292402e43fb6a6d77e753cf8147ab5160d99
tykozidane2015 <tykozidane2015@gmail.com>
2022-06-14 11:53:14 +0700

    user again

--- models/Users.js
+++ models/Users.js
@@ -1,14 +1,17 @@
-const mongoose = require("mongoose")
+const mongoose = require("mongoose");
 
-const userSchema = new mongoose.Schema({
-    username: {type: String , required:true, unique:true},
-    email: {type: String , required:true , unique:true},
+const userSchema = new mongoose.Schema(
+  {
+    username: { type: String, required: true, unique: true },
+    email: { type: String, required: true, unique: true },
     password: { type: String, required: true },
-    phone: { type: String, required:true },
+    phone: { type: String, required: true },
     alamat: { type: Array },
-    tanggallahir: {type: Date, default: " "},
-    jeniskelamin: {type: String, default: " "},
+    tanggallahir: { type: Date },
+    jeniskelamin: { type: String, default: " " },
     role: { type: String, default: "user" },
-}, { timestamps: true});
+  },
+  { timestamps: true }
+);
 
 module.exports = mongoose.model("Users", userSchema);
